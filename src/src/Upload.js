a80b4926c6efea344b343843fa856c539e0d6f8a
tranthang <tranthangusername@gmail.com>
2023-12-02 23:22:23 +0700

    add Upload page

---
+++ src/src/Upload.js
@@ -0,0 +1,47 @@
+import "./Upload.css";
+
+export const Upload = () => {
+  return (
+    <div className="homepage-SV">
+            <div className="overlap-wrapper">
+                <div className="overlap">
+                  <div className="frame">
+                    <div className="overlap1">
+                    <div className="rectangle" />
+                      <p className="text-wrapper10">Hoặc kéo thả tệp ở đây</p>
+                      <div className="group">
+                        <div className="overlap-group">
+                          <div className="div">
+                            <div className="text-wrapper11">Chọn tệp</div>
+                            <img className="down-svgrepo" alt="Down svgrepo" src="https://i.imgur.com/6HqhQky.png" />
+                            <div className="line"/>
+                          </div>
+                          <img className="image" alt="Image" src="https://i.imgur.com/TJjadhH.png" />
+                        </div>
+                      </div>
+                      <img className="upload-file" alt="Upload file" src="https://i.imgur.com/JJMrZvq.png" />
+                    </div>
+                    <p className="p">Tải tài liệu lên để in</p>
+                  </div> 
+                    <div className="heading">
+                        <div className="navbar">
+                            <div className="text-wrapper-6">Lịch sử</div>
+                            <div className="text-wrapper-7">Mua Trang</div>
+                            <div className="text-wrapper-8">In</div>
+                            <div className="text-wrapper-9">Trang chủ</div>
+                        </div>
+                        <div className="user-icon">
+                            <img className="search-icon" alt="Search icon" src="https://i.imgur.com/VLWKDmO.png" />
+                            <img className="customer-support" alt="Customer support" src="https://i.imgur.com/gTfcuZY.png" />
+                            <img className="bell" alt="Bell" src="https://i.imgur.com/nPrio7M.png" />
+                            <img className="img" alt="User icon" src="https://i.imgur.com/0V7oQzx.png" />
+                            <img className="expand-arrow" alt="Expand arrow" src="https://i.imgur.com/wMo6W9J.png" />
+                        </div>
+                        <img className="logobk" alt="Logobk" src="https://i.imgur.com/3ZUlxdA.png" />
+                    </div>
+                </div>
+            </div>
+        </div>
+  );
+};
+export default Upload;
