0f60af42a1f3086783428878b86ccb127bb59975
Loskarr <143611763+Loskarr@users.noreply.github.com>
2023-12-03 21:36:26 +0700

    Add properties

--- source_code/src/App.js
+++ source_code/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import StudentHomepage from "./page/StudentHomepage"
 import Login from "./page/Login"
 import Upload from "./page/Upload"
+import Properties from "./page/Properties"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="StudentHomepage" element={<StudentHomepage />} />
         <Route path="Upload" element={<Upload />} />
+        <Route path="Properties" element={<Properties />} />
       </Routes>
     </div>
   )
