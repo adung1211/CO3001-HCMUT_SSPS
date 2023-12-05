c4b85845e5df4ad488be456f1acc2d3130ac12d5
Pham Anh Dung <dungphamanh2003@gmail.com>
2023-12-05 17:34:04 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomeStudent from "./pages/HomeStudent";
 import Login from "./pages/Login";
 import Print from "./pages/Print";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" exact element={<Login />} />
         <Route path="/home" exact element={<HomeStudent />} />
         <Route path="/login" exact element={<Login />} />
         <Route path="/print" exact element={<Print />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
