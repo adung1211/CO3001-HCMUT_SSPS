2b566a3d2a1b64be2bc13aa08f79f05de802ef1c
Pham Anh Dung <dungphamanh2003@gmail.com>
2023-12-08 07:57:24 +0700

    add filter for choosing printer

--- src/pages/ChoosePrinter.js
+++ src/pages/ChoosePrinter.js
@@ -8,35 +8,60 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+
+import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import Tooltip from "@mui/material/Tooltip";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 import MenuBar from "../components/MenuBar";
 
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-
-const PrinterList = [
-  { id: 1, name: "Canon PIXMA MG3620", coso: "Lý Thường Kiệt" },
-  { id: 2, name: "Brother HLL2380DW", coso: "Thủ Đức" },
-  { id: 3, name: "HP LaserJet Pro MFP M29W", coso: "Lý Thường Kiệt" },
-  { id: 4, name: "Epson EcoTank ET4760", coso: "Lý Thường Kiệt" },
-  { id: 5, name: "Samsung Xpress C430W", coso: "Thủ Đức" },
-  { id: 6, name: "Brother HLL2380DW", coso: "Thủ Đức" },
-  { id: 7, name: "Brother HLL2380DW", coso: "Lý Thường Kiệt" },
-  { id: 8, name: "Brother HLL2380DW", coso: "Lý Thường Kiệt" },
-  { id: 9, name: "Xerox Phaser 6510DN", coso: "Thủ Đức" },
+const DefaultList = [
+  { id: 1, name: "Canon PIXMA MG3620", fac: "Lý Thường Kiệt", build: "A2" },
+  { id: 2, name: "Brother HLL2380DW", fac: "Dĩ An", build: "H1" },
+  { id: 3, name: "Brother HLL2380DW", fac: "Lý Thường Kiệt", build: "A2" },
+  { id: 4, name: "Epson EcoTank ET4760", fac: "Lý Thường Kiệt", build: "C3" },
+  { id: 5, name: "Samsung Xpress C430W", fac: "Dĩ An", build: "H1" },
+  { id: 6, name: "Brother HLL2380DW", fac: "Dĩ An", build: "H6" },
+  { id: 7, name: "Brother HLL2380DW", fac: "Lý Thường Kiệt", build: "B4" },
+  { id: 8, name: "Brother HLL2380DW", fac: "Lý Thường Kiệt", build: "B4" },
+  { id: 9, name: "Xerox Phaser 6510DN", fac: "Dĩ An", build: "H3" },
 ];
-const filters = [{ text: "Lọc theo toà" }];
 export default function ChoosePrinter() {
-  const [anchorFilter, setAnchorFilter] = React.useState(null);
-  const handleCloseFilter = (event) => {
-    setAnchorFilter(null);
+  const [filterFac, setFilterFac] = React.useState("");
+  const [filterBuild, setFilterBuild] = React.useState("");
+  const [PrinterList, setPrinterList] = React.useState(DefaultList);
+
+  const handleChangeFac = (event) => {
+    const selectedFac = event.target.value;
+
+    // Filter the PrinterList based on the selected facility
+    const filteredPrinters = DefaultList.filter(
+      (printer) => printer.fac === selectedFac
+    );
+
+    // Update the state with the filtered facility and clear the filterBuild
+    setFilterFac(selectedFac);
+    setPrinterList(filteredPrinters);
+    if (selectedFac === "") {
+      setPrinterList(DefaultList);
+    }
+    setFilterBuild("");
   };
-  const handleOpenFilter = (event) => {
-    setAnchorFilter(event.currentTarget);
+  const handleChangeBuild = (event) => {
+    const selectedBuild = event.target.value;
+
+    // Filter the PrinterList based on the selected building
+    const filteredPrinters = PrinterList.filter(
+      (printer) => printer.build === selectedBuild
+    );
+
+    // Update the state with the filtered building and clear the filterBuild
+    setFilterBuild(selectedBuild);
+    setPrinterList(filteredPrinters);
+    if (selectedBuild === "") {
+      setPrinterList(DefaultList);
+    }
   };
   const ShowPrinter = (val) => {
     return (
@@ -46,7 +71,7 @@ export default function ChoosePrinter() {
             <CardMedia
               component="img"
               sx={{ width: 151 }}
-              image="https://i.imgur.com/Ma9RIGO.png"
+              image="https://lh3.googleusercontent.com/6ywT4BKNphnw3yBHAO-2p-gjW3Bj--puzTV-E7N9qEZNbp0-AuS5xeDjl54ndl5bKiZ4ypwbkbe-RpgBqqAh=w500-rw"
               alt="May in"
             />
             <Box sx={{ display: "flex", flexDirection: "column" }}>
@@ -56,8 +81,8 @@ export default function ChoosePrinter() {
                 </Typography>
                 <Typography>
                   ID: {val.id} <br />
-                  Cơ sở: {val.coso} <br />
-                  Toà: <br />
+                  Cơ sở: {val.fac} <br />
+                  Toà: {val.build} <br />
                   Phòng: <br />
                 </Typography>
                 <Typography
@@ -79,35 +104,63 @@ export default function ChoosePrinter() {
   return (
     <>
       <MenuBar />
-      <Box sx={{ ml: 150, mb: 5, mt: 2 }}>
-        <Tooltip title="Open settings">
-          <Button
-            id="demo-customized-button"
-            variant="outlined"
-            onClick={handleOpenFilter}
-            endIcon={<KeyboardArrowDownIcon />}
-          >
-            Options
-          </Button>
-        </Tooltip>
-        <Menu
-          sx={{ ml: -2 }}
-          id="menu-appbar"
-          anchorEl={anchorFilter}
-          keepMounted
-          open={Boolean(anchorFilter)}
-          onClose={handleCloseFilter}
-        >
-          {filters.map((filters) => (
-            <MenuItem
-              key={filters}
-              onClick={() => handleCloseFilter(filters.text)}
+      <Stack
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        direction="row"
+        sx={{ ml: 90, mb: 2, mt: 2 }}
+      >
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel>Cơ sở</InputLabel>
+          <Select value={filterFac} label="Cơ sở" onChange={handleChangeFac}>
+            <MenuItem value="">None</MenuItem>
+            <MenuItem value={"Lý Thường Kiệt"}>Lý Thường Kiệt</MenuItem>
+            <MenuItem value={"Dĩ An"}>Dĩ An</MenuItem>
+          </Select>
+        </FormControl>
+        {filterFac === "Dĩ An" && (
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-helper-label">Toà</InputLabel>
+            <Select
+              value={filterBuild}
+              label="Toà"
+              onChange={handleChangeBuild}
             >
-              <Typography textAlign="center">{filters.text}</Typography>
-            </MenuItem>
-          ))}
-        </Menu>
-      </Box>
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"H1"}>H1</MenuItem>
+              <MenuItem value={"H2"}>H2</MenuItem>
+              <MenuItem value={"H3"}>H3</MenuItem>
+              <MenuItem value={"H6"}>H6</MenuItem>
+            </Select>
+          </FormControl>
+        )}
+        {filterFac === "Lý Thường Kiệt" && (
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel>Toà</InputLabel>
+            <Select
+              value={filterBuild}
+              label="Toà"
+              onChange={handleChangeBuild}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"A2"}>A2</MenuItem>
+              <MenuItem value={"B3"}>B3</MenuItem>
+              <MenuItem value={"C4"}>C4</MenuItem>
+            </Select>
+          </FormControl>
+        )}
+        {filterFac === "" && (
+          <FormControl sx={{ m: 1, minWidth: 120 }} disabled>
+            <InputLabel>Toà</InputLabel>
+            <Select value={filterBuild} label="Toà"></Select>
+          </FormControl>
+        )}
+      </Stack>
       <Box display="flex" justifyContent="center" alignItems="center">
         {" "}
         <Box
