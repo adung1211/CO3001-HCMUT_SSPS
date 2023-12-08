b4dae8d2665d3501166cd0f4a940a23051f07514
Pham Anh Dung <dungphamanh2003@gmail.com>
2023-12-08 10:23:42 +0700

    update choose printer page

--- src/pages/ChoosePrinter.js
+++ src/pages/ChoosePrinter.js
@@ -8,6 +8,10 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import Modal from "@mui/material/Modal";
 
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -17,21 +21,89 @@ import Select from "@mui/material/Select";
 import MenuBar from "../components/MenuBar";
 
 const DefaultList = [
-  { id: 1, name: "Canon PIXMA MG3620", fac: "Lý Thường Kiệt", build: "A2" },
-  { id: 2, name: "Brother HLL2380DW", fac: "Dĩ An", build: "H1" },
-  { id: 3, name: "Brother HLL2380DW", fac: "Lý Thường Kiệt", build: "A2" },
-  { id: 4, name: "Epson EcoTank ET4760", fac: "Lý Thường Kiệt", build: "C3" },
-  { id: 5, name: "Samsung Xpress C430W", fac: "Dĩ An", build: "H1" },
-  { id: 6, name: "Brother HLL2380DW", fac: "Dĩ An", build: "H6" },
-  { id: 7, name: "Brother HLL2380DW", fac: "Lý Thường Kiệt", build: "B4" },
-  { id: 8, name: "Brother HLL2380DW", fac: "Lý Thường Kiệt", build: "B4" },
-  { id: 9, name: "Xerox Phaser 6510DN", fac: "Dĩ An", build: "H3" },
+  {
+    id: 1,
+    name: "Canon PIXMA MG3620",
+    fac: "Lý Thường Kiệt",
+    build: "A2",
+    room: "201",
+    state: "Sẵn sàng",
+  },
+  {
+    id: 2,
+    name: "Brother HLL2380DW",
+    fac: "Dĩ An",
+    build: "H1",
+    room: "305",
+    state: "Sẵn sàng",
+  },
+  {
+    id: 3,
+    name: "Brother HLL2380DW",
+    fac: "Lý Thường Kiệt",
+    build: "A2",
+    room: "201",
+    state: "Không sẵn sàng",
+  },
+  {
+    id: 4,
+    name: "Epson EcoTank ET4760",
+    fac: "Lý Thường Kiệt",
+    build: "C3",
+    room: "212",
+    state: "Sẵn sàng",
+  },
+  {
+    id: 5,
+    name: "Samsung Xpress C430W",
+    fac: "Dĩ An",
+    build: "H1",
+    room: "602",
+    state: "Sẵn sàng",
+  },
+  {
+    id: 6,
+    name: "Brother HLL2380DW",
+    fac: "Dĩ An",
+    build: "H6",
+    room: "307",
+    state: "Sẵn sàng",
+  },
+  {
+    id: 7,
+    name: "Brother HLL2380DW",
+    fac: "Lý Thường Kiệt",
+    build: "B4",
+    room: "102",
+    state: "Sẵn sàng",
+  },
+  {
+    id: 8,
+    name: "Brother HLL2380DW",
+    fac: "Lý Thường Kiệt",
+    build: "B4",
+    room: "102",
+    state: "Không sẵn sàng",
+  },
+  {
+    id: 9,
+    name: "Xerox Phaser 6510DN",
+    fac: "Dĩ An",
+    build: "H3",
+    room: "107",
+    state: "Sẵn sàng",
+  },
 ];
 export default function ChoosePrinter() {
+  const navigate = useNavigate();
   const [filterFac, setFilterFac] = React.useState("");
   const [filterBuild, setFilterBuild] = React.useState("");
   const [PrinterList, setPrinterList] = React.useState(DefaultList);
+  const [choosen, setChoosen] = React.useState("");
 
+  const handleChoose = (val) => {
+    setChoosen(val.id);
+  };
   const handleChangeFac = (event) => {
     const selectedFac = event.target.value;
 
@@ -63,15 +135,75 @@ export default function ChoosePrinter() {
       setPrinterList(DefaultList);
     }
   };
+  const [open, setOpen] = React.useState(false);
+  const [openError, setOpenError] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    if (choosen === "") {
+      setOpenError(true);
+    } else {
+      setOpen(true);
+      setTimeout(() => {
+        navigate("/home");
+      }, 2000);
+    }
+  };
   const ShowPrinter = (val) => {
     return (
       <Grid item xs={4}>
-        <CardActionArea>
-          <Card sx={{ display: "flex" }}>
+        {val.state === "Sẵn sàng" && (
+          <CardActionArea onClick={() => handleChoose(val)}>
+            <Card
+              sx={
+                val.id === choosen
+                  ? {
+                      display: "flex",
+                      boxShadow: "0 0 0 0.5rem #1488DB",
+                    }
+                  : {
+                      display: "flex",
+                    }
+              }
+            >
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image="https://i.imgur.com/94qk77J.jpg"
+                alt="May in"
+              />
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" fontWeight="bold" variant="h6">
+                    {val.name}
+                  </Typography>
+                  <Typography>
+                    ID: {val.id} <br />
+                    Cơ sở: {val.fac} <br />
+                    Toà: {val.build} <br />
+                    Phòng: {val.room} <br />
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "green",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {" "}
+                    Sẵn sàng
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Card>
+          </CardActionArea>
+        )}
+        {val.state === "Không sẵn sàng" && (
+          <Card sx={{ display: "flex", backgroundColor: "#E9E8E8" }}>
             <CardMedia
               component="img"
               sx={{ width: 151 }}
-              image="https://lh3.googleusercontent.com/6ywT4BKNphnw3yBHAO-2p-gjW3Bj--puzTV-E7N9qEZNbp0-AuS5xeDjl54ndl5bKiZ4ypwbkbe-RpgBqqAh=w500-rw"
+              image="https://i.imgur.com/94qk77J.jpg"
               alt="May in"
             />
             <Box sx={{ display: "flex", flexDirection: "column" }}>
@@ -83,21 +215,20 @@ export default function ChoosePrinter() {
                   ID: {val.id} <br />
                   Cơ sở: {val.fac} <br />
                   Toà: {val.build} <br />
-                  Phòng: <br />
+                  Phòng: {val.room} <br />
                 </Typography>
                 <Typography
                   sx={{
-                    color: "green",
+                    color: "#9A5151",
                     fontWeight: "bold",
                   }}
                 >
-                  {" "}
-                  Sẵn sàng
+                  Không sẵn sàng
                 </Typography>
               </CardContent>
             </Box>
           </Card>
-        </CardActionArea>
+        )}
       </Grid>
     );
   };
@@ -172,7 +303,7 @@ export default function ChoosePrinter() {
             overflowY: "scroll",
           }}
         >
-          <Grid container spacing={2}>
+          <Grid container spacing={2} p={1}>
             {PrinterList.map((printer) => (
               <ShowPrinter key={printer.id} {...printer} />
             ))}
@@ -187,13 +318,42 @@ export default function ChoosePrinter() {
         direction="row"
         spacing={2}
       >
-        <Button variant="contained" color="error">
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => navigate("/home")}
+        >
           Huỷ
         </Button>
-        <Button variant="contained" color="success">
+        <Button variant="contained" color="success" onClick={handleOpen}>
           Chọn
         </Button>
       </Stack>
+      <Backdrop
+        sx={{ color: "#1488DB", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        transitionDuration={{ exit: 1000, enter: 1000, appear: 1000 }}
+        onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Modal open={openError} onClose={() => setOpenError(false)}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #fff",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography>Bạn chưa chọn máy in</Typography>
+        </Box>
+      </Modal>
     </>
   );
 }
